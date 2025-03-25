82e91280214f052d22e5d01cf5ae5762de5f701d
sahnoon <sahnoonmohamuud506@gmail.com>
2025-03-25 15:00:44 +0300

    add project entry files

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";  // ✅ Import global CSS
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const CLIENT_ID ="484415681658-q9gm0qsr2adtnrm2erk00kkmndg92230.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );

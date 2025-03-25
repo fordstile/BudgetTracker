bae11d00049c7664404f77b52b2a47b0d724785d
sahnoon <sahnoonmohamuud506@gmail.com>
2025-03-25 15:09:26 +0300

    add avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Avatar.css";
+
+
+const Avatar = ({ name, image }) => {
+  return (
+    <div className="user-avatar">
+      <img src={image} alt="User Avatar" className="avatar-image" />
+      <div className="user-info">
+        <p className="greeting">Hello, {name} 👋</p>
+        <p className="username">{name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;

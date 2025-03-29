2d5714efd51680465d474e48cdd248d108e679b6
fordstile <masterland9772@gmail.com>
2025-03-29 18:34:37 +0300

    add assets directory for static files

---
+++ src/assets/assets.js
@@ -0,0 +1,41 @@
+import arrow_icon from "./arrow_icon.svg";
+import lock_icon from "./lock_icon.svg";
+import logo from "./logo.svg";
+import mail_icon from "./mail_icon.svg";
+import person_icon from "./person_icon.svg";
+import hand_wave from "./hand_wave.png";
+import image1 from "./image1.jpg";
+import image2 from "./image2.jpg";
+import image3 from "./image3.jpg";
+import image4 from "./image4.jpg";
+import image5 from "./image5.jpg";
+import image6 from "./image6.jpg";
+
+import image8 from "./image8.jpg";
+import phone from "./phone.png";
+import user1 from "./user1.jpeg";
+import user2 from "./user2.jpeg";
+import user3 from "./user4.jpeg";
+import user4 from "./user4.jpeg";
+
+export const assets = {
+  arrow_icon,
+  lock_icon,
+  logo,
+  mail_icon,
+  person_icon,
+  hand_wave,
+  phone,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+
+  image8,
+  user1,
+  user2,
+  user3,
+  user4,
+};
